124fe50408d3b09f3ec2cc16bc36b2bf282c4178
remyhny <posremy@gmail.com>
2015-06-16 00:10:40 +0200

    preparation des pages

---
+++ App/app.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+var app = angular.module('chat', [
+        'ngRoute'
+    ])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'Modules/Login/loginView.html',
+            controller: 'LoginCtrl'
+        });
+        $routeProvider.otherwise({ redirectTo: '/' });
+    }]);
---
+++ server.js
@@ -0,0 +1,29 @@
+﻿var conf = require('./conf.json');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+
+
+var httpServer = http.createServer(onRequest).listen(conf.http.port);
+
+//Fonction onRequest est exécutée à chaque requête sur le serveur
+function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var extension = pathname.split('.').pop();
+    if (pathname == '/') {
+        pathname = conf.http.index;
+    }
+    response.writeHead(200, {
+        'Content-Type': conf.http.mime[extension],
+        'Access-Control-Allow-Origin': '*'
+    });
+    try {
+        response.end(fs.readFileSync(conf.http.www + pathname));
+    } catch (e) {
+        response.writeHead(302, {
+            'Location': 'http://localhost:5555/'
+        });
+        response.end();
+    }
+}
